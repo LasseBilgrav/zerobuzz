f664c210c71b8ba80d3bc486e187952a0592b76b
LasseBilgrav <eaa24lsbi@students.eaaa.dk>
2025-03-04 10:19:38 +0100

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -26,11 +26,7 @@ function createCheckoutContainer() {
     checkoutContainer.innerHTML = `
         <div class="cart-header">
             <div class="cart-toggle">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <circle cx="9" cy="21" r="1"></circle>
-                    <circle cx="20" cy="21" r="1"></circle>
-                    <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
-                </svg>
+                <img src="../img/shopping-cart.svg" alt="Shopping Cart" class="cart-icon">
                 <span class="cart-count">0</span>
             </div>
             <button class="minimize-button">◀</button> <!-- Ændret til venstre-pil som standard -->
@@ -71,6 +67,12 @@ function createCheckoutContainer() {
             margin-right: 1rem;
             height: 40px; /* Explicitly set a small height when minimized */
             overflow: hidden;
+        }
+
+        .cart-icon {
+            width: 24px;
+            height: 24px;
+            margin-right: 8px;
         }
         
         .cart-header {
